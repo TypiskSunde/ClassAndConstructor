da28d7d1a63979ff3cb838ba9ecc2c6593d751e1
TypiskSunde <80268206+TypiskSunde@users.noreply.github.com>
2022-03-16 08:54:48 +0100

    Add project files.

---
+++ ClassAndConstructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ClassAndConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lag en konsollapplikasjon med en klasse som heter TextPrinter. Klassen TextPrinter skal ha en public objektvariabel som heter Name.
+            //Lag en constructor til klassen som tar inn et parameter string name. I constructoren kan du sette variabelen Name = name.
+            //Denne klassen skal ha en metode PrintName som skriver ut Name til konsollen.
+            //Lag også en metode PrintText() som looper igjennom en for-løkke 10 ganger og printer til konsollen “Runde nr” +nummeret den befinner seg på i løkken.
+            //Lag en ny instans av TextPrinter i program.cs filen, og kall på PrintName() og PrintText() metodene.
+
+            var textPrinter = new TextPrinter("Name");
+            textPrinter.PrintName();
+            textPrinter.PrintText();
+        }
+        
+    }
+}
---
+++ ClassAndConstructor/TextPrinter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndConstructor
+{
+    class TextPrinter
+    {
+        public string Name;
+        
+
+        public TextPrinter(string name)
+        {
+            Name = name;
+          
+           
+        }
+
+        public void PrintName()
+        {
+            Console.WriteLine(Name);
+        }
+
+        public void PrintText()
+        {
+           
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.WriteLine($"Runde nr  {i+1}");
+                }
+                
+          
+           
+        }
+
+    }
+    
+}
